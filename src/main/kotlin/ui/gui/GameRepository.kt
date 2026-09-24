95c404e28dfd5f94bcd949fa24d4d1d63e355976
DolzhenkoAlexa <dolzhenko.sasha2006@mail.ru>
2026-09-24 03:22:48 +0300

    add better start of a programm

--- src/main/kotlin/ui/gui/GameRepository.kt
+++ src/main/kotlin/ui/gui/GameRepository.kt
@@ -1,7 +1,8 @@
 package main.kotlin.ui.gui
 
-import main.kotlin.history.repository.InMemoryRepository
+import main.kotlin.history.repository.Repository
+import main.kotlin.history.repository.SqliteRepository
 
 object GameRepository {
-    val instance: InMemoryRepository = InMemoryRepository()
+    val instance: Repository = SqliteRepository()
 }
